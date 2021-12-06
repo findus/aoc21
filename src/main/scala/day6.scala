c977c5eba3c2e16111c1281b13a61cef70159d8f
Philipp <philipp@f1ndus.de>
2021-12-06 20:35:53 +0100

    clusterfuck

--- src/main/scala/day6.scala
+++ src/main/scala/day6.scala
@@ -8,10 +8,7 @@ object day6 extends App {
     val o = numbers.groupBy(f => f.toLong).view.mapValues(_.size.toLong).toMap
     def plusDay(map: Map[Long,Long]): Map[Long,Long] = {
       val e = map.map(entry => (entry._1 - 1, entry._2)).toMap
-      val reproduce = e.getOrElse(-1L,0L)
-      val newMap = e.filter(e => e._1 != -1 && e._1 != 6).+ ((8,reproduce))
-      val newnewMap = newMap.+((6L,e.getOrElse(6,0L)+e.getOrElse(-1,0L)))
-      newnewMap
+      e.filter(e => e._1 != -1 && e._1 != 6).+ ((8,e.getOrElse(-1L,0L))).+((6L,e.getOrElse(6,0L)+e.getOrElse(-1,0L)))
     }
     println((1 to 80).foldLeft(o)((prev,_) => plusDay(prev)).values.sum)
     println((1 to 256).foldLeft(o)((prev,_) => plusDay(prev)).values.sum)
