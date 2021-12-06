472c2145f7e67e27a3dbbc3f91cf9afd5c06fbe3
Philipp <philipp@f1ndus.de>
2021-12-06 20:34:22 +0100

    day6 part2

--- src/main/scala/day6.scala
+++ src/main/scala/day6.scala
@@ -1,27 +1,22 @@
 import scala.io.Source
 
-case class Lanternfish(var state: Int) {
-  def life(): List[Lanternfish] = {
-    state -= 1;
-    if (state < 0) {
-      state = 6
-      return List(Lanternfish(8));
-    }
-    List.empty
-  }
-
-  override def toString(): String = {
-    state.toString
-  }
-}
-
 object day6 extends App {
   val filename = "src/main/resources/day6"
   val file = Source.fromFile(filename)
-  val fishes = file.getLines().toList.head.split(",").map(number => Lanternfish(number.toInt));
-  println((1 to 80).foldLeft(fishes)((prev, _) => prev ++ prev.flatMap(fishes => fishes.life())).toList.size)
- // println((1 to 256).foldLeft(fishes)((prev, _) => prev ++ prev.flatMap(fishes => fishes.life())).toList.size)
-
+  val numbers = file.getLines().toList.head.split(",")
+  def partx() = {
+    val o = numbers.groupBy(f => f.toLong).view.mapValues(_.size.toLong).toMap
+    def plusDay(map: Map[Long,Long]): Map[Long,Long] = {
+      val e = map.map(entry => (entry._1 - 1, entry._2)).toMap
+      val reproduce = e.getOrElse(-1L,0L)
+      val newMap = e.filter(e => e._1 != -1 && e._1 != 6).+ ((8,reproduce))
+      val newnewMap = newMap.+((6L,e.getOrElse(6,0L)+e.getOrElse(-1,0L)))
+      newnewMap
+    }
+    println((1 to 80).foldLeft(o)((prev,_) => plusDay(prev)).values.sum)
+    println((1 to 256).foldLeft(o)((prev,_) => plusDay(prev)).values.sum)
+  }
+  partx()
 }
 
 
