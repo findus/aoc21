e0814c096105ecf02512eba3169aef6f4bdc089c
Philipp <philipp@f1ndus.de>
2021-12-07 17:00:25 +0100

    day 7 part 2

--- src/main/scala/day7.scala
+++ src/main/scala/day7.scala
@@ -4,9 +4,6 @@ object day7 extends App {
   val filename = "src/main/resources/day7"
   val file = Source.fromFile(filename)
   val crabPosition = file.getLines().toList.head.split(",").map(e => e.toInt)
-  println(crabPosition.toList)
-  val e = (0 to crabPosition.length).map(e => crabPosition.map(cp => {
-    (cp - e).abs
-  })).toList
-  println(e.map(f => f.sum).min)
+  println((0 to crabPosition.length).map(e => crabPosition.map(cp => (cp - e).abs)).map(f => f.sum).min)
+  println((0 to crabPosition.length).map(e => crabPosition.map(cp => (1 until (cp - e).abs + 1).sum)).map(f => f.sum).min)
 }
