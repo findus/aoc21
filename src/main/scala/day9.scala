325c887b9fc2d687710b220bedc24a768c75571c
Philipp <philipp@f1ndus.de>
2021-12-10 01:13:37 +0100

    Day9 Part2

--- src/main/scala/day9.scala
+++ src/main/scala/day9.scala
@@ -1,3 +1,6 @@
+import day9.cursor
+
+import scala.annotation.tailrec
 import scala.io.Source
 
 object day9 extends App {
@@ -5,25 +8,9 @@ object day9 extends App {
   val file = Source.fromFile(filename)
   val lines = file.getLines().toList
 
-  def orElse(e: Int) = -1
-
-  def upperHigher(passedHeight: Int, upperList: List[Int], position: Int) = {
-    upperList(position) > passedHeight
-  }
-
-  def lowerHigher(passedHeight: Int, lowerList: List[Int], position: Int) = {
-    lowerList(position) > passedHeight
-  }
-
-  def lowerSameList(passedPosition: Int, list: List[Int]) = {
-    list.applyOrElse(passedPosition - 1, orElse) > list.applyOrElse(passedPosition, orElse) && passedPosition % 10 != 0 &&
-      list.applyOrElse(passedPosition, orElse) < list.applyOrElse(passedPosition + 1, orElse)
-  }
-
   val listOfLines = lines.map(line => line.map(e => e.asDigit).toList).toList
 
-
-  def check(cursor: (Int,Int)) = {
+  def check(cursor: (Int,Int)): (Boolean,Option[(Int,Int)]) = {
     val value = listOfLines(cursor._1)(cursor._2)
     val leftVal = if (cursor._2 != 0)                           { listOfLines(cursor._1)(cursor._2 -1) }   else { -1 }
     val rightVal = if (cursor._2 < listOfLines.head.length -1)  { listOfLines(cursor._1)(cursor._2+ 1) }   else { -1 }
@@ -31,13 +18,27 @@ object day9 extends App {
     val botVal = if (cursor._1 < listOfLines.length - 1)        { listOfLines(cursor._1 + 1)(cursor._2) }  else { -1 }
     val result = (value < leftVal || leftVal == -1) && (value < rightVal || rightVal == -1 ) && (value < topVal || topVal == -1) && (value < botVal || botVal == -1)
 
-    if (result) {
+/*    if (result) {
       println(s"   $topVal ")
       println(s" $leftVal $value $rightVal ")
       println(s"  $botVal ")
-    }
+    }*/
 
-    result
+    (result,Some(cursor))
+  }
+
+  def getNeighboursBelow9(cursor: (Int,Int), ignore: List[(Int,Int)]): List[(Int,Int)] = {
+
+    val leftVal = if (cursor._2 != 0)                           { Some((listOfLines(cursor._1)(cursor._2 -1), (cursor._1,cursor._2 -1))) } else {  None }
+    val rightVal = if (cursor._2 < listOfLines.head.length -1)  { Some((listOfLines(cursor._1)(cursor._2+ 1), (cursor._1,cursor._2 + 1))) } else { None }
+    val topVal = if (cursor._1 != 0)                            { Some((listOfLines(cursor._1 - 1)(cursor._2),(cursor._1 - 1,cursor._2))) } else { None }
+    val botVal = if (cursor._1 < listOfLines.length - 1)        { Some((listOfLines(cursor._1 + 1)(cursor._2),(cursor._1 + 1,cursor._2)))} else { None }
+    val list = List(leftVal,rightVal,topVal,botVal).filter(e => e.isDefined).filter(e => e.get._1 >= 0 && e.get._1 < 9).map(e => e.get).filter(e => {
+      !ignore.contains(e._2)
+    }).filter(pp => pp._2 != cursor)
+
+    val e = list.foldLeft(List.empty[(Int,Int)])((prev,data) => getNeighboursBelow9(data._2, (prev.++(ignore)).distinct.++(list.map(e=>e._2)).distinct))
+    e.++(ignore).distinct
   }
 
 
@@ -51,9 +52,20 @@ object day9 extends App {
     x <- 0 to xLen
     y <- 0 to yLen
   } yield {
-    if (check((x,y))) { Some(1 + listOfLines(x)(y)) } else { None }
+    if (check((x,y))._1) { Some(1 + listOfLines(x)(y)) } else { None }
   }
+  println("Part1: ",e.filter(e => e.isDefined).map(e => e.get).sum)
 
+  val f = for {
+    x <- 0 to xLen
+    y <- 0 to yLen
+  } yield {
+    val c = check((x,y))
+    if (c._1) { c._2 } else { None }
+  }
+  val basinscenter = f.filter(e => e.isDefined).map(e => e.get)
 
-  println(e.filter(e => e.isDefined).map(e => e.get).sum)
+  val test = basinscenter.head
+  val test2 = basinscenter.map(entry => getNeighboursBelow9(entry,List(entry)))
+  println(test2.map(e => e.length).sorted.reverse.slice(0,3).product)
 }
