9b805a92ed84242d51b185194c69491e6d9d4041
Philipp <philipp@f1ndus.de>
2021-12-18 17:36:22 +0100

    day 13 part2

--- src/main/scala/day13.scala
+++ src/main/scala/day13.scala
@@ -14,6 +14,32 @@ object day13 extends App {
     FoldInstruction(data(0),data(1).toInt)
   })
 
+  def fold(instruction: FoldInstruction,coordinates: List[Coordinate]) = {
+    if (instruction.along == "y") {
+      val at = instruction.at
+      coordinates.foldLeft(List.empty[Coordinate])((prev,data) => {
+        if (at - data.y > 0) {
+          prev.::(data)
+        } else {
+          val amount = if  (at - data.y == -1) { -2 } else { ((at - data.y) * 2)}
+          val newY = data.y + amount
+          prev.::(Coordinate(data.x, newY))
+        }
+      }).distinct
+    } else {
+      val at = instruction.at
+      coordinates.foldLeft(List.empty[Coordinate])((prev,data) => {
+        if (at - data.x > 0) {
+          prev.::(data)
+        } else {
+          val amount = if  (at - data.x == -1) { -2 } else { ((at - data.x) * 2)}
+          val newX = data.x + amount
+          prev.::(Coordinate(newX, data.y))
+        }
+      }).distinct
+    }
+  }
+
   val coordinates = coords.map(coord => { Coordinate(coord.split(",")(0).toInt, coord.split(",")(1).toInt) })
 
   println(coordinates)
@@ -24,32 +50,32 @@ object day13 extends App {
 
   val firstFold = foldInstructions.head
 
-  val folded = if (firstFold.along == "y") {
-    val at = firstFold.at
-    coordinates.foldLeft(List.empty[Coordinate])((prev,data) => {
-      if (at - data.y > 0) {
-        prev.::(data)
-      } else {
-        val amount = if  (at - data.y == -1) { -2 } else { ((at - data.y) * 2)}
-        val newY = data.y + amount
-        prev.::(Coordinate(data.x, newY))
-      }
-    }).distinct
-  } else {
-    val at = firstFold.at
-    coordinates.foldLeft(List.empty[Coordinate])((prev,data) => {
-      if (at - data.x > 0) {
-        prev.::(data)
-      } else {
-        val amount = if  (at - data.x == -1) { -2 } else { ((at - data.x) * 2)}
-        val newX = data.x + amount
-        prev.::(Coordinate(newX, data.y))
-      }
-    }).distinct
-  }
+  val folded = fold(firstFold,coordinates)
 
+  //Part1
   println(folded.length)
   println(folded)
 
+  //Part2
+  val folded2 = foldInstructions.foldLeft(coordinates)((prev,data) => fold(data, prev))
+  val maxY = folded2.map(e => e.y).max
+  val maxX = folded2.map(e => e.x).max
 
+  var lastY = 0
+  for {
+    y <- (0 to maxY)
+    x <- (0 to maxX)
+  } {
+    if (lastY != y) {
+      println("")
+      lastY = y
+    }
+    if (folded2.contains(Coordinate(x,y))) {
+      print("X")
+    } else {
+      print(" ")
+    }
+  }
+  println(folded2.length)
+  println(folded2)
 }
