e6d0b1c4c5636d0811e4ef8fec8041b9f2009e91
Philipp <philipp@f1ndus.de>
2021-12-19 14:59:04 +0100

    Day 16 start

---
+++ src/main/scala/day16.scala
@@ -0,0 +1,5 @@
+object day16 extends App {
+  val input = "VVVTTTILLLLLLLLLLLLLLLAAAAAAAAAAABBBBBBBBBBBBBBBB"
+  val binaryInput = input.toCharArray.map(chr => Integer.parseInt(chr.toString,16)).toList
+  binaryInput
+}
